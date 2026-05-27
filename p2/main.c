4988f85e31ca7c3969b74dbeebeb74d93389cbb2
2025050052 <junkyeom@kau.kr>
2026-05-27 15:43:54 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -3,22 +3,25 @@
 int main()
 {
     int N;
-    int array1[20]={};
-    int array2[20]={};
+    int array1[20] = {};
+    int array2[20] = {};
 
-    
-    scanf("%d\n",&N);
-    for(int i=0;i<N;i++) {
-        scanf("%d ",&array1[i]);
+    int *p1 = array1;
+    int *p2 = array2;
+
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", p1 + i); 
     }
 
-    
-    for(int i=0;i<N;i++) {
-        scanf("%d ",&array2[i]);
+    for(int i = 0; i < N; i++) {
+        scanf("%d", p2 + i); 
     }
 
-    for(int i=0;i<N;i++) {
-        printf(" %d",array1[i]+array2[N-1-i]);
+    for(int i = 0; i < N; i++) {
+        printf(" %d", *(p1 + i) + *(p2 + N - 1 - i));
     }
     
+    return 0;
 }
